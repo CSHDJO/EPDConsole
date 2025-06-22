69ef4d4863f1a3f984fa3a11a9e109cde2f4b0e6
Wael Orraby <orraby2000@gmail.com>
2025-06-22 13:43:08 +0200

    Verbeter de structuur van de EPDDbContext klasse door de namespace en klasse-definitie te vereenvoudigen; behoud de DbSet-eigenschappen en de override van SaveChangesAsync voor asynchrone opslag.

--- Chipsoft.Assignments.EPDConsole.Infrastructure/Persistence/EPDDbContext.cs
+++ Chipsoft.Assignments.EPDConsole.Infrastructure/Persistence/EPDDbContext.cs
@@ -2,21 +2,16 @@ using Chipsoft.Assignments.EPDConsole.Core.Entities;
 using Chipsoft.Assignments.EPDConsole.Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
-namespace Chipsoft.Assignments.EPDConsole.Infrastructure.Persistence
+namespace Chipsoft.Assignments.EPDConsole.Infrastructure.Persistence;
+
+public class EPDDbContext(DbContextOptions<EPDDbContext> options) : DbContext(options), IApplicationDbContext
 {
-    public class EPDDbContext : DbContext, IApplicationDbContext
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Physician> Physicians { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
     {
-        public EPDDbContext(DbContextOptions<EPDDbContext> options) : base(options)
-        {
-        }
-
-        public DbSet<Patient> Patients { get; set; }
-        public DbSet<Physician> Physicians { get; set; }
-        public DbSet<Appointment> Appointments { get; set; }
-
-        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-        {
-            return await base.SaveChangesAsync(cancellationToken);
-        }
+        return await base.SaveChangesAsync(cancellationToken);
     }
-} 
+}
