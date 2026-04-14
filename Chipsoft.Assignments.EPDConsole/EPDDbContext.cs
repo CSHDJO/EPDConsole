815999e45aa26adde0142603af8cf92c5a51d6c2
Hendrik <hendrikdejonge@hotmail.com>
2026-04-14 13:26:41 +0200

    Update .net

--- Chipsoft.Assignments.EPDConsole/EPDDbContext.cs
+++ Chipsoft.Assignments.EPDConsole/EPDDbContext.cs
@@ -5,13 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chipsoft.Assignments.EPDConsole
+namespace Chipsoft.Assignments.EPDConsole;
+
+public class EPDDbContext : DbContext
 {
-    public class EPDDbContext : DbContext
-    {
-        // The following configures EF to create a Sqlite database file in the
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source=epd.db");
-        public DbSet<Patient> Patients { get; set; }
-    }
+    // The following configures EF to create a Sqlite database file in the
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source=epd.db");
+
+    public DbSet<Patient> Patients { get; set; }
 }
